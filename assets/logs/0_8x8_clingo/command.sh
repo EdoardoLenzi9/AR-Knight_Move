928263dafcaf26eeba603580dabe801644967404
Edoardo <edoardolenzi9@gmail.com>
2021-01-02 11:49:14 +0100

    clean logs

--- assets/logs/0_8x8_clingo/command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#clingo asp/knights_tour.lp -c n=8 --time-limit=300
-clingo /home/eddy/Note/Automated_Reasoning/AR-Knights_Tour/assets/logs/0_8x8_clingo/knights_tour.lp /home/eddy/Note/Automated_Reasoning/AR-Knights_Tour/assets/logs/0_8x8_clingo/database.lp -c n=8 --time-limit=10
